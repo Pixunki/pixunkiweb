97776c686bc412611e1a5d7a3efaf23f7d948ace
mazunki <rolferen@gmail.com>
2019-12-27 17:39:52 +0100

    second commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ web_app = flask.Flask(__name__)
 
 @web_app.route("/<name>/<content>")
 @web_app.route("/<name>")
+@web_app.route("/")
 def home(name=None, content=None):
     return flask.render_template('default_template.html', name=name, content=content)
 
