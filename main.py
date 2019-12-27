a302fb2b328b51946d3d8313d52fb53129761626
mazunki <rolferen@gmail.com>
2019-12-27 18:01:30 +0100

    kwargs func

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ web_app = flask.Flask(__name__)
 @web_app.route("/<name>/<content>")
 @web_app.route("/<name>")
 @web_app.route("/")
-def home(name=None, content=None, morecontent=None):
-    return flask.render_template('default_template.html', name=name, content=content, morecontent=morecontent)
+def home(**kwargs):
+    return flask.render_template('default_template.html', **kwargs)
 
 
 if __name__ == '__main__':
