d044b966144304c066dcd06ed773f4134c324822
pixelexio <rogerio@snarli.no>
2019-12-27 17:53:14 +0100

    pixfirst commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import flask
 web_app = flask.Flask(__name__)
 
+@web_app.route("/<name>/<content>/<morecontent>")
 @web_app.route("/<name>/<content>")
 @web_app.route("/<name>")
 @web_app.route("/")
-def home(name=None, content=None):
-    return flask.render_template('default_template.html', name=name, content=content)
+def home(name=None, content=None, morecontent=None):
+    return flask.render_template('default_template.html', name=name, content=content, morecontent=morecontent)
 
 
 if __name__ == '__main__':
