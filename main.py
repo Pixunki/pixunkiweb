693933cdaeb607eb7f368248e1664e1ac2be87b6
mazunki <rolferen@gmail.com>
2019-12-27 21:44:33 +0100

    added style for menues and dynamic imports

--- main.py
+++ main.py
@@ -8,11 +8,23 @@ test = {
     "teachers": "boring"
 }
 
-@web_app.route("/<folder>/<file>")
-@web_app.route("/<folder>/")
+@web_app.route("/home.html")
 @web_app.route("/")
 def home(**kwargs):
-    return fl.render_template('default_template.html', people=test, **kwargs)
+    return fl.render_template('home.html', people=test, **kwargs)
+
+@web_app.route("/neopixels.html")
+def run_neopixels():
+    return fl.render_template('neopixels.html')
+
+@web_app.route("/people.html")
+def people():
+    return fl.render_template('people.html')
+
+@web_app.route("/runFunction")
+def button_action():
+    print("We did it, cowards!")
+    return ""
 
 
 if __name__ == '__main__':
