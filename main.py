5c84ab28b7c39e58a18d765eb40e341aa84c5867
mazunki <rolferen@gmail.com>
2019-12-27 18:04:53 +0100

    rename import

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import flask
-web_app = flask.Flask(__name__)
+import flask as fl
+web_app = fl.Flask(__name__)
 
 @web_app.route("/<name>/<content>/<morecontent>")
 @web_app.route("/<name>/<content>")
 @web_app.route("/<name>")
 @web_app.route("/")
 def home(**kwargs):
-    return flask.render_template('default_template.html', **kwargs)
+    return fl.render_template('default_template.html', **kwargs)
 
 
 if __name__ == '__main__':
