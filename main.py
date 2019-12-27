1cb6698c87149cd012d22413e7aa01d34869d1be
mazunki <rolferen@gmail.com>
2019-12-27 19:50:23 +0100

    beauty

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import flask as fl
 web_app = fl.Flask(__name__)
 
-@web_app.route("/<name>/<content>/<morecontent>")
-@web_app.route("/<name>/<content>")
-@web_app.route("/<name>")
+@web_app.route("/<folder>/<file>")
+@web_app.route("/<folder>/")
 @web_app.route("/")
 def home(**kwargs):
     return fl.render_template('default_template.html', **kwargs)
