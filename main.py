ff02c16b9060e67268f8fe1436721ddf6274bc96
mazunki <rolferen@gmail.com>
2019-12-27 20:54:54 +0100

    sample tabular data

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import flask as fl
 web_app = fl.Flask(__name__)
 
+test = {
+    "rogerio": "cool",
+    "potatoes": "yummy",
+    "mazunki": "hungwy",
+    "teachers": "boring"
+}
+
 @web_app.route("/<folder>/<file>")
 @web_app.route("/<folder>/")
 @web_app.route("/")
 def home(**kwargs):
-    return fl.render_template('default_template.html', **kwargs)
+    return fl.render_template('default_template.html', people=test, **kwargs)
 
 
 if __name__ == '__main__':
